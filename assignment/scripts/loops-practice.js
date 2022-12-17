af67b6a51c7f322817d92c306b2672400fd73b96
Andrea Love <andrealovelovelove@gmail.com>
2022-12-17 17:53:03 -0600

    Finished question 1

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -11,20 +11,36 @@ for (let i=0; i<4; i++) {
 }
 
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
-//   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+for (let i=0; i<6; i++) {
+  console.log(i);
+}
+//   - Which part of the example loop do you need to change to do this?
+//   - Need to change the number that i is less than to do this
 
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
-//   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
+for (let i=3; i<6; i++) {
+  console.log(i);
+}
+//   - Which part of the example loop do you need to change to do this?
+//   - Need to change the i=__, in this case i=3
 
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
-//   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
+for (let i=2; i<11; i+=2) {
+    console.log(i);
+}
+//   - Which part of the example loop do you need to change to do this?
+//   - Have to chagne the starting value of i, the maximum value of i, and the increment number
 
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i=5; i>=0; i--){
+  console.log(i);
+}
+//   - Which part of the example loop do you need to change to do this?
+//   - Have to change starting value of i, set i less than or equal to 0, and increment switched to decrement
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
